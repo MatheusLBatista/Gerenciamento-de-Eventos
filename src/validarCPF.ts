1c16c9377c463c5f529aed210c24e8d831ca0fca
Matheus Lucas Batista <matheusifro2020@gmail.com>
2024-10-25 15:32:11 -0400

    code done

--- src/validarCPF.ts
+++ src/validarCPF.ts
@@ -1,19 +1,24 @@
-let cpf: string = "52149730200";
+let cpf: string = "01162809213";
 
 function validarCPF(cpf: string){
+
+    if(cpf.length !== 11){
+        console.log("CPF inválido!");
+        return false;
+    }
 
     let cpfNumber = cpf.split('').map(Number).slice(0, 9);
     let somaPrimeiro: number = 0;
     let somaSegundo: number = 0;
 
-    for(let i = 0 ; i <= cpfNumber.length -1; i ++){
+    for(let i = 0 ; i < cpfNumber.length; i ++){
         let multiplicaPrimeiro: number = cpfNumber[i] * (10 - i);
         somaPrimeiro += multiplicaPrimeiro;
     }
 
     somaPrimeiro = somaPrimeiro % 11;
     somaPrimeiro = 11 - somaPrimeiro;
-    somaPrimeiro >= 10 ? somaPrimeiro = 0 : somaPrimeiro;
+    somaPrimeiro = somaPrimeiro >= 10 ? somaPrimeiro = 0 : somaPrimeiro;
 
     cpfNumber.push(somaPrimeiro);
 
@@ -24,7 +29,7 @@ function validarCPF(cpf: string){
 
     somaSegundo = somaSegundo % 11;
     somaSegundo = 11 - somaSegundo;
-    somaSegundo >= 10 ? somaSegundo = 0 : somaSegundo;
+    somaSegundo = somaSegundo >= 10 ? somaSegundo = 0 : somaSegundo;
   
     cpfNumber.push(somaSegundo);
 
