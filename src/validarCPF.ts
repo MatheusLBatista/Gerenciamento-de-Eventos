3f2c65a2a7af7b07f215a9d612d1a5a8b4407a48
MatheusLBatista <matheusifro2020@gmail.com>
2024-10-24 21:21:00 -0400

    calculando digitos verificadores cpf

--- src/validarCPF.ts
+++ src/validarCPF.ts
@@ -1,11 +1,13 @@
 let cpf: string = "71633057291";
+//cpf.split('').map(Number);
+//console.log(cpf[5]);
 
 
 function validarCPF(cpf: string){
 
     let digitoVerificador: Number = 0;
     let verificadorPrimeiro = [10, 9, 8, 7, 6, 5, 4, 3, 2];
-    let cpfNumber = cpf.split('').map(Number)
+    let cpfNumber = cpf.split('').map(Number).slice(0, 9);
     let somaPrimeiro = 0;
 
     for(let i = 0 ; i <= verificadorPrimeiro.length - 1 ; i ++){
@@ -17,11 +19,33 @@ function validarCPF(cpf: string){
     somaPrimeiro = somaPrimeiro % 11;
     somaPrimeiro = 11 - somaPrimeiro;
     console.log(somaPrimeiro) 
+
+    cpfNumber.push(somaPrimeiro);
+    console.log(cpfNumber)
+
+    let verificadorSegundo = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+    let somaSegundo = 0;
+
+    for(let i = 0; i < verificadorSegundo.length; i++){
+        let multiplicaSegundo = cpfNumber[i] * verificadorSegundo[i];
+        somaSegundo = somaSegundo + multiplicaSegundo;
+    }
+
+    somaSegundo = somaSegundo % 11;
+    somaSegundo = 11 - somaSegundo;
+    
+    cpfNumber.push(somaSegundo);
+    console.log(cpfNumber)
+
+    /*if(somaPrimeiro == cpf[9] && somaSegundo == cpf[10]){
+
+    }*/
+
 }
 
 validarCPF(cpf);
 
-
+//posso apenas verificar se os digitos são iguais, sem precisar do push e dividir o array
 
 
 
